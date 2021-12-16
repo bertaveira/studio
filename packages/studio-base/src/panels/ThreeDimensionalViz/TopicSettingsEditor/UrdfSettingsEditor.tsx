8be009564240c1b530965b3231731848968aa6c4
Roman Shtylman <roman@foxglove.dev>
2021-12-16 10:42:36 -0800

    Omit package:// references from URDF settings editor in web
    Web does not support loading package:// urls so we do not display this as a hint to the user.
    
    Fixes: #2395

--- packages/studio-base/src/panels/ThreeDimensionalViz/TopicSettingsEditor/UrdfSettingsEditor.tsx
+++ packages/studio-base/src/panels/ThreeDimensionalViz/TopicSettingsEditor/UrdfSettingsEditor.tsx
@@ -11,31 +11,37 @@
 //   found at http://www.apache.org/licenses/LICENSE-2.0
 //   You may not use this file except in compliance with the License.
 
+import { TextField } from "@fluentui/react";
+
 import Flex from "@foxglove/studio-base/components/Flex";
+import isDesktopApp from "@foxglove/studio-base/util/isDesktopApp";
 
 import { TopicSettingsEditorProps } from ".";
-import { SLabel, SDescription, SInput } from "./common";
 
 export type UrdfSettings = {
   urdfUrl?: string;
 };
+
+// One day we can think about using feature detection. Until that day comes we acknowledge the
+// realities of only having two playforms: web and desktop.
+const supportsPackageUrl = isDesktopApp();
 
 export default function UrdfSettingsEditor(
   props: TopicSettingsEditorProps<undefined, UrdfSettings>,
 ): React.ReactElement {
   const { settings = {}, onFieldChange } = props;
 
+  const descriptionString = supportsPackageUrl
+    ? "package:// URL or http(s) URL pointing to a Unified Robot Description Format (URDF) XML file"
+    : "http(s) URL pointing to a Unified Robot Description Format (URDF) XML file";
+
   return (
     <Flex col>
-      <SLabel>URDF Location</SLabel>
-      <SDescription>
-        package:// URL or http(s) URL pointing to a Unified Robot Description Format (URDF) XML file
-      </SDescription>
-      <SInput
-        type="text"
+      <TextField
+        label={descriptionString}
         value={settings.urdfUrl ?? ""}
-        onChange={(e) => {
-          onFieldChange("urdfUrl", e.target.value);
+        onChange={(_, newValue) => {
+          onFieldChange("urdfUrl", newValue);
         }}
       />
     </Flex>
